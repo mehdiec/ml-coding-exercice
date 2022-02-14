d2c7b5abbd746a6d3e6910d534be13ef2fa0d27f
Ech-Chouini Mehdi <mehdi.ech@student-cs.fr>
2022-02-14 07:43:17 +0100

    [+] Train file

--- src/train.py
+++ src/train.py
@@ -1,25 +1,80 @@
-from sklearn.model_selection import train_test_split
+import argparse
+import os
+import pickle
+import yaml
+from shutil import copyfile
 from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 from src.data.load import train_loader
 
 
-image_size = 28
-num_images = 60000
+def generate_unique_logpath(logdir, raw_run_name):
+    """Verify if the path already exist
 
-##################################################### loading #####################################################
+    Args:
+        logdir (str): path to log dir
+        raw_run_name (str): name of the file
 
-images, targets = train_loader(num_images=num_images)
-targets = targets[:num_images]
+    Returns:
+        str: path to the output file
+    """
+    i = 0
+    while True:
+        run_name = raw_run_name + "_" + str(i)
+        log_path = os.path.join(logdir, run_name)
+        if not os.path.isdir(log_path):
+            return log_path
+        i = i + 1
 
-##################################################### training #####################################################
 
-train_images, test_images, train_labels, test_labels = train_test_split(
-    images, targets, train_size=0.8, random_state=0
-)
-print(len(train_images))
+def main(cfg, path_to_config):
+    """Main pipeline to train a ML model
 
-clf = svm.SVC()
-clf.fit(train_images, train_labels)
-clf.score(test_images, test_labels)
-print(clf.score(test_images, test_labels))
+    Args:
+        cfg (dict): config with all the necessary parameters
+        path_to_config(string): path to the config file
+    """
+    # Load data
+    cfg["DATASET"]["VALID_RATIO"]
+    num_images = cfg["DATASET"]["num_images"]
+    images, targets = train_loader(num_images=num_images)
+    targets = targets[:num_images]
+
+    train_images, test_images, train_labels, test_labels = train_test_split(
+        images, targets, train_size=cfg["DATASET"]["VALID_RATIO"], random_state=0
+    )
+
+    # Init directory to save model saving best models
+
+    top_logdir = cfg["TRAIN"]["SAVE_DIR"]
+    save_dir = generate_unique_logpath(top_logdir, cfg["MODELS"]["ML"]["TYPE"].lower())
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    copyfile(path_to_config, os.path.join(save_dir, "config_file.yaml"))
+
+    model = svm.SVC()
+    model.fit(train_images, train_labels)
+    pickle.dump(model, open(os.path.join(save_dir, "model.pck"), "wb"))
+
+    print("Accuaracy : ", model.score(test_images, test_labels))
+
+
+if __name__ == "__main__":
+    # Init the parser
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+    # Add path to the config file to the command line arguments
+    parser.add_argument(
+        "--path_to_config",
+        type=str,
+        required=True,
+        default="./config.yaml",
+        help="path to config file",
+    )
+    args = parser.parse_args()
+
+    with open(args.path_to_config, "r") as ymlfile:
+        config_file = yaml.load(ymlfile, Loader=yaml.Loader)
+
+    main(cfg=config_file, path_to_config=args.path_to_config)
